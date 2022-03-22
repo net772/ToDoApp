87f23559423ef4edb9a56d698c523a6bb7e02271
donghyun <kdhyun000@naver.com>
2022-03-22 15:24:40 +0900

    패키지 셋팅 및 koin셋팅

---
+++ app/src/main/java/com/example/todoapp/Application.kt
@@ -0,0 +1,22 @@
+package com.example.todoapp
+
+import android.app.Application
+import com.example.todoapp.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class Application: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@Application)
+            modules(appModule)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/todoapp/di/appModule.kt
@@ -0,0 +1,6 @@
+package com.example.todoapp.di
+
+import org.koin.dsl.module
+
+internal val appModule = module {
+}
